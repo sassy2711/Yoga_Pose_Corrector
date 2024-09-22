4495e7d3cb62c21b6f11a1c7750f6fdd13007190
Shashwat Chaturvedi <shashwatchaturvedi271104@gmail.com>
2024-09-23 01:12:31 +0530

    put all backend files inside a folder

---
+++ backend/PoseModule.py
@@ -0,0 +1,187 @@
+import cv2 as cv
+import mediapipe as mp
+import time
+import numpy as np
+
+
+class PoseDetector:
+    def __init__(self, mode=False, upBody=False, smooth=True,
+                  detectionCon = 0.5, trackCon=0.5):
+        self.mode = mode
+        self.upBody = upBody
+        self.smooth = smooth
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose()   # I was getting some errors while passing the parameters here so i didn't pass them here
+
+        self.mpDraw = mp.solutions.drawing_utils
+
+
+    def findPose(self, frame, draw = True):
+        self.imgRGB = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        self.results = self.pose.process(self.imgRGB)
+        if self.results.pose_landmarks:
+            if draw:
+                landmark_style = self.mpDraw.DrawingSpec(color=(0,0,255), thickness=2, circle_radius=2)
+                connection_style = self.mpDraw.DrawingSpec(color=(255,255,255), thickness=2)
+                self.mpDraw.draw_landmarks(frame, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS,landmark_drawing_spec=landmark_style, connection_drawing_spec=connection_style)
+        
+        return frame
+    
+    def findPosition(self, frame, draw = False):
+        lmlist = []
+        if self.results.pose_landmarks:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                h, w, c = frame.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmlist.append([lm.x, lm.y])
+                if draw:
+                    cv.putText(frame, str(id), (cx, cy), cv.FONT_HERSHEY_PLAIN, 1.0, (255,255,255), thickness=2)
+        return lmlist
+    
+    def ChangeColor(self, frame, color=(0,255,0), draw=True):
+        if self.results.pose_landmarks:
+            if draw:
+                landmark_style = self.mpDraw.DrawingSpec(color=(0,0,255), thickness=2, circle_radius=2)
+                connection_style = self.mpDraw.DrawingSpec(color=color, thickness=2)
+                self.mpDraw.draw_landmarks(frame, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS,landmark_drawing_spec=landmark_style, connection_drawing_spec=connection_style)
+        
+        return frame
+    
+    def map_landmarks(self, landmarks):
+        landmark_dict = {
+        'nose': landmarks[0],
+        'left_eye_inner': landmarks[1],
+        'left_eye': landmarks[2],
+        'left_eye_outer': landmarks[3],
+        'right_eye_inner': landmarks[4],
+        'right_eye': landmarks[5],
+        'right_eye_outer': landmarks[6],
+        'left_ear': landmarks[7],
+        'right_ear': landmarks[8],
+        'mouth_left': landmarks[9],
+        'mouth_right': landmarks[10],
+        'left_shoulder': landmarks[11],
+        'right_shoulder': landmarks[12],
+        'left_elbow': landmarks[13],
+        'right_elbow': landmarks[14],
+        'left_wrist': landmarks[15],
+        'right_wrist': landmarks[16],
+        'left_pinky': landmarks[17],
+        'right_pinky': landmarks[18],
+        'left_index': landmarks[19],
+        'right_index': landmarks[20],
+        'left_thumb': landmarks[21],
+        'right_thumb': landmarks[22],
+        'left_hip': landmarks[23],
+        'right_hip': landmarks[24],
+        'left_knee': landmarks[25],
+        'right_knee': landmarks[26],
+        'left_ankle': landmarks[27],
+        'right_ankle': landmarks[28],
+        'left_heel': landmarks[29],
+        'right_heel': landmarks[30],
+        'left_foot_index': landmarks[31],
+        'right_foot_index': landmarks[32],
+        'mid_hip': ((landmarks[23][0]+landmarks[24][0])/2, (landmarks[23][1]+landmarks[24][1])/2),
+        'neck': ((landmarks[11][0]+landmarks[12][0])/2, (landmarks[11][1]+landmarks[12][1])/2)
+        
+    }
+        return landmark_dict
+    
+    def map_joints(self, landmark_dict):
+        joint_dict = {
+        "left_knee_joint": [landmark_dict['left_ankle'], landmark_dict['left_knee'], landmark_dict['left_hip']],
+        "right_knee_joint": [landmark_dict['right_ankle'], landmark_dict['right_knee'], landmark_dict['right_hip']],
+        "left_hip_joint": [landmark_dict['left_knee'], landmark_dict['left_hip'], landmark_dict['left_shoulder']],
+        "right_hip_joint": [landmark_dict['right_knee'], landmark_dict['right_hip'], landmark_dict['right_shoulder']],
+        "left_shoulder_joint": [landmark_dict['left_elbow'], landmark_dict['left_shoulder'], landmark_dict['left_hip']],
+        "right_shoulder_joint": [landmark_dict['right_elbow'], landmark_dict['right_shoulder'], landmark_dict['right_hip']],
+        "neck": [landmark_dict['mid_hip'], landmark_dict['neck'], landmark_dict['nose']],
+        "leg_angle": [landmark_dict['left_knee'], landmark_dict['mid_hip'], landmark_dict['right_knee']],
+        "left_ankle_joint": [landmark_dict['left_knee'], landmark_dict['left_ankle'], landmark_dict['left_foot_index']],
+        "right_ankle_joint": [landmark_dict['right_knee'], landmark_dict['right_ankle'], landmark_dict['right_foot_index']],
+        "left_elbow": (landmark_dict['left_shoulder'], landmark_dict['left_elbow'], landmark_dict['left_wrist']),
+        "right_elbow": (landmark_dict['right_shoulder'], landmark_dict['right_elbow'], landmark_dict['right_wrist'])
+    }
+        return joint_dict
+
+    def map_asana_joints(self):
+        asana_to_joint={}
+        asana_to_joint["pranamasana"]=["left_elbow","left_shoulder_joint","left_hip_joint","left_knee_joint"]
+        asana_to_joint["hastauttanasana"]=["left_elbow","left_shoulder_joint","left_hip_joint","left_knee_joint","neck"]
+        asana_to_joint["hastapadasana"]=["left_elbow","left_shoulder_joint","left_hip_joint","left_knee_joint"]
+        asana_to_joint["right_ashwa_sanchalanasana"]=["left_elbow","left_shoulder_joint","left_hip_joint","left_knee_joint","right_knee_joint","neck"]
+        asana_to_joint["dandasana"]=["left_elbow","left_shoulder_joint","left_hip_joint","left_knee_joint","neck"]
+        asana_to_joint["ashtanga_namaskara"]=["left_hip_joint","left_knee_joint","left_ankle_joint"]
+        asana_to_joint["bhujangasana"]=["left_elbow","left_shoulder_joint","left_hip_joint","left_knee_joint","neck"]
+        asana_to_joint["adho_mukha_svanasana"]=["left_elbow","left_shoulder_joint","left_hip_joint","left_knee_joint","neck"]
+        asana_to_joint["left_ashwa_sanchalanasana"]=["left_elbow","left_shoulder_joint","left_hip_joint","left_knee_joint","right_knee_joint","neck"]
+        return asana_to_joint
+    
+    def get_joints_for_asana(self,asana,asana_to_joint,joint_dict):
+        new_joint_dict={}
+        for i in asana_to_joint[asana]:
+            new_joint_dict[i]=joint_dict[i]
+        return new_joint_dict
+    
+    def calculate_angle(self, points):
+        """
+        Calculate the angle between three points a, b, and c.
+        a: The first point (shoulder).
+        b: The vertex point (elbow).
+        c: The second point (wrist).
+        """
+        if(points != None):
+            a = np.array(points[0])  # Shoulder
+            b = np.array(points[1])  # Elbow
+            c = np.array(points[2])  # Wrist
+
+        # Calculate the vectors
+            ba = a - b  # Vector from shoulder to elbow
+            bc = c - b  # Vector from elbow to wrist
+
+                # Calculate the angle using the dot product
+            cosine_angle = np.dot(ba, bc) / (np.linalg.norm(ba) * np.linalg.norm(bc))
+            angle = np.arccos(cosine_angle)  # Angle in radians
+
+            return np.degrees(angle)  # Convert to degrees
+        else:
+            print("No points given")
+    
+    
+
+    
+
+
+
+        
+            
+
+
+    
+
+
+def main():
+    vid = cv.VideoCapture(-1)
+    ptime = 0
+    ctime = 0 
+    detector = PoseDetector()
+
+    while True:
+        isTrue, frame = vid.read()
+        frame = detector.findPose(frame)
+        lmlist = detector.findPosition(frame)
+
+
+        ctime = time.time()
+        fps = 1/(ctime-ptime)
+        ptime = ctime
+        cv.putText(frame, str(f'{int(fps)} FPS'), (30, 40), cv.FONT_HERSHEY_PLAIN, 1.7, (0,255,0), thickness=3)
+        cv.imshow('Video', frame)
+
+        cv.waitKey(10)
+
+if __name__ == "__main__":
+    main()
---
+++ backend/pose_equal_check.py
@@ -0,0 +1,146 @@
+import numpy as np
+import cv2 as cv
+import mediapipe as mp
+import numpy as np
+import time
+import PoseModule as pm
+# Initialize MediaPipe Pose Detection
+import ideal_landmarks_data
+import absolutely_ideal_landmarks_data
+
+ideal_landmarks = ideal_landmarks_data.ideal_landmarks
+absolutely_ideal_landmarks = absolutely_ideal_landmarks_data.absolutely_ideal_landmarks
+detector = pm.PoseDetector()
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose()
+mp_drawing = mp.solutions.drawing_utils
+asana_to_joint = detector.map_asana_joints()
+ctime =0
+ptime = time.time()
+class PoseSimilarity():
+    # Function to calculate Euclidean distance between two points
+    def euclidean_distance(self, point1, point2):
+        return np.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
+    
+    # Function to normalize key points based on a reference point
+    def normalize_landmarks(self, landmarks, reference_idx):
+        ref_point = landmarks[reference_idx]
+        normalized_landmarks = [(point[0] - ref_point[0], point[1] - ref_point[1]) for point in landmarks]
+        return normalized_landmarks
+    
+    # Function to compare two sets of pose landmarks
+    def compare_poses(self, landmarks1, landmarks2, threshold=0.1):
+        total_distance = 0
+        for i in range(len(landmarks1)):
+            total_distance += self.euclidean_distance(landmarks1[i], landmarks2[i])
+        avg_distance = total_distance / len(landmarks1)
+        return avg_distance
+    
+    def get_wrong_joints(self, asana, correct_landmarks, input_landmarks, thresh):
+        correct_landmark_dict = detector.map_landmarks(correct_landmarks)
+        correct_joints_dict = detector.map_joints(correct_landmark_dict)
+        correct_joints_dict=detector.get_joints_for_asana(asana,asana_to_joint,correct_joints_dict)
+
+        input_landmark_dict = detector.map_landmarks(input_landmarks)
+        input_joints_dict = detector.map_joints(input_landmark_dict)
+        input_joints_dict=detector.get_joints_for_asana(asana,asana_to_joint,input_joints_dict)
+
+        wrong_joints = {}
+        for i in correct_joints_dict:
+            correct_angle = detector.calculate_angle(correct_joints_dict[i])
+            input_angle = detector.calculate_angle(input_joints_dict[i])
+            diff = correct_angle - input_angle
+            if(abs(diff)>thresh):
+                if(diff>0):
+                    wrong_joints[i] =  (i, "increase")
+                else:
+                    wrong_joints[i] = (i, "decrease")
+        return wrong_joints
+    
+    def isSimilar(self, pose_name, input_landmarks, euclidean_threshold):
+        correct_landmarks = ideal_landmarks[pose_name] + absolutely_ideal_landmarks[pose_name]
+        mini = float('inf')
+        closest_landmarks = []
+        flag = 0
+        for i in correct_landmarks:
+            dist = self.compare_poses(i, input_landmarks, euclidean_threshold)
+            if(dist<euclidean_threshold):
+                print("You're doing it right.")
+                flag = 1
+            if(dist<mini):
+                mini = dist
+                closest_landmarks = i
+        
+        if(flag):
+            return (True, closest_landmarks)
+        else:            
+            return (False, closest_landmarks)
+        #return self.get_wrong_joints(pose_name, closest_landmarks, input_landmarks, angular_threshold)
+        
+
+def resize_image(image, max_width=800, max_height=600):
+    height, width = image.shape[:2]
+    if width > max_width or height > max_height:
+        scaling_factor = min(max_width / width, max_height / height)
+        new_size = (int(width * scaling_factor), int(height * scaling_factor))
+        return cv.resize(image, new_size)
+    return image
+
+
+
+
+if __name__ == "__main__":
+    # Example usage
+    pose_sim = PoseSimilarity()
+    
+    
+
+    # Example usage with your images
+    frame1 = cv.imread("Padmasana.jpeg")
+    frame1 = resize_image(frame1)
+    #cv.imshow("Without_detection", frame1_resized)
+    frame1 = detector.findPose(frame1)
+    lmlist1 = detector.findPosition(frame1)
+    frame2 = cv.imread("correct_padmasana.jpeg")
+    frame2 = resize_image(frame2)
+    frame2 = detector.findPose(frame2)
+    lmlist2 = detector.findPosition(frame2)
+    #cv.imshow("Corrected_padmasana", frame1)
+    #cv.imshow("Wrong_padmasana", frame2)
+    #Example landmarks for two poses (these should be passed in from another part of your code)
+    frame_rgb1 = cv.cvtColor(frame1, cv.COLOR_BGR2RGB)
+    result1 = pose.process(frame_rgb1)
+    landmarks1 = []
+    if result1.pose_landmarks:
+        for lm in result1.pose_landmarks.landmark:
+            landmarks1.append((lm.x, lm.y))
+
+    frame_rgb2 = cv.cvtColor(frame2, cv.COLOR_BGR2RGB)
+    result2 = pose.process(frame_rgb2)
+    landmarks2 = []
+    if result2.pose_landmarks:
+        for lm in result2.pose_landmarks.landmark:
+            landmarks2.append((lm.x, lm.y))
+    
+    # print("landmarks1: ", landmarks1)
+    # print("lmlist1: ", lmlist1)
+    # print("landmarks2: ", landmarks2)
+    # print("lmlist2: ", lmlist2)
+
+
+
+
+    # Normalize landmarks with respect to a reference point (e.g., left hip index = 24)
+    normalized_landmarks1 = pose_sim.normalize_landmarks(lmlist1, reference_idx=0)
+    normalized_landmarks2 = pose_sim.normalize_landmarks(lmlist2, reference_idx=0)
+    
+    # Compare the poses
+    wrong_joints = pose_sim.get_wrong_joints(normalized_landmarks1, normalized_landmarks2, 20)
+    if(pose_sim.compare_poses(normalized_landmarks1, normalized_landmarks2, 0.1)):
+        print("Correct")
+    else:
+        for i in wrong_joints:
+            print(i, wrong_joints[i])
+    ctime = time.time()
+    print(ctime-ptime)
+
